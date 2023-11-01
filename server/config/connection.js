1786dc8f4bf2347731eec7905558a2d14587fd31
nabeelyash12345 <nabeel.gilgit123456@gmail.com>
2023-11-01 16:30:25 +0000

    server connected

---
+++ server/config/connection.js
@@ -0,0 +1,16 @@
+const mongoose =require("mongoose")
+
+
+const Url="mongodb+srv://nabeelahmad12345:nabeelahmad12345@cluster0.4bl8pjt.mongodb.net/"
+mongoose.connect(Url,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+
+}).then(()=>{
+    console.log("db connected")
+})
+
+const db=mongoose.connection;
+db.on("error",console.error.bind(console,"connection failed"))
+
+module.exports=mongoose
