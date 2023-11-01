fd8b722d8242680a5b741de64406bd16dde821e6
nabeelyash12345 <nabeel.gilgit123456@gmail.com>
2023-11-01 20:48:45 +0000

    server change

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,7 @@
 const mongoose =require("mongoose")
 
-// nabeelgilgit123456
-// XS7lXROmIFmsihhJ
+
 const Url="mongodb+srv://nabeelgilgit123456:XS7lXROmIFmsihhJ@cluster0.4majehp.mongodb.net/"
-// const Url="mongodb+srv://nabeelahmad12345:nabeelahmad12345@cluster0.4bl8pjt.mongodb.net/"
-
-
 
 mongoose.connect(Url,{
     useNewUrlParser:true,
